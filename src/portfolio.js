9ab72f88ff852306b021f227ca2dff4afff6447a
Fathy <fathymohsen47@gmail.com>
2022-09-12 15:00:03 +0200

    Update

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,20 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://github.com/fathymo7',
+  title: 'FM.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Fathy Mohsen',
+  role: 'Front End Developer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    'Web developer with a proven ability to collaborate effectively with senior developers while spending extra time to be mentored. Enjoy working closely with team members to ensure workloads are effectively redirected to bottlenecks and personally picking up the slack when necessary. With a passion for both personal growth and technical growth. ',
+  resume:
+    'https://drive.google.com/file/d/1q3I1ieqnRAR_aDcsAb4JYBgrx3UTOXas/view?usp=sharing',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://linkedin.com/in/fathy-mohsen',
+    github: 'https://github.com/fathymo7',
   },
 }
 
@@ -21,51 +22,48 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Food Order Project',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'A page showing items which a user can order, choose the quantity and cancel order at any time. The project is using ReactJs and fetched by RestfulAPI from google firebase. ',
+    stack: ['HTML', 'CSS', 'ReactJs', 'API'],
+    sourceCode: 'https://github.com/fathymo7/Food-Application',
   },
   {
-    name: 'Project 2',
+    name: 'Quotes Project',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'In this project, a user can add a quote which is displayed in quotes page while other users can comment on it. This project is using React-Router and fetched by RestfulAPI from google firebase.',
+    stack: ['HTML', 'CSS', 'ReactJs', 'React-Router'],
+    sourceCode: 'https://github.com/fathymo7/Quotes-Application',
   },
   {
-    name: 'Project 3',
+    name: 'Redux Cart',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'A page showing Items and the user can pick items to cart and remove items from it. This project using is Redux hooks and fetched by RestfulAPI from google firebase.',
+    stack: ['HTML', 'CSS', 'ReactJs', 'React-Router'],
+    sourceCode: 'https://github.com/fathymo7/Redux-Cart',
   },
 ]
 
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
-  'HTML',
-  'CSS',
+  'HTML5',
+  'CSS3',
   'JavaScript',
-  'TypeScript',
-  'React',
+  'ReactJs',
   'Redux',
-  'SASS',
-  'Material UI',
+  'React Router',
   'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
+  'React Native',
+  'Restful API',
+  'MongoDB',
+  'NextJs',
+  'TailwindCSS',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'fathymohsen97@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
